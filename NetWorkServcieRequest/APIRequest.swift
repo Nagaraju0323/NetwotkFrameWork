b7009165a0551bf304ab9a7fa843a898ae81e968
Nagaraju0323 <nagarajukios85@gmail.com>
2024-10-14 23:12:42 +0530

    Latest

---
+++ NetWorkServcieRequest/APIRequest.swift
@@ -0,0 +1,15 @@
+//
+//  APIRequest.swift
+//  NetWorkServcieRequest
+//
+//  Created by Nagaraju on 14/10/24.
+//
+
+import Foundation
+
+public struct HTTPRequest {
+    static func apiRequest() {
+        print("WelcomeToRequest")
+    }
+}
+    
---
+++ NetWorkServcieRequest/NetWorkServcieRequest.h
@@ -0,0 +1,18 @@
+//
+//  NetWorkServcieRequest.h
+//  NetWorkServcieRequest
+//
+//  Created by Nagaraju on 14/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetWorkServcieRequest.
+FOUNDATION_EXPORT double NetWorkServcieRequestVersionNumber;
+
+//! Project version string for NetWorkServcieRequest.
+FOUNDATION_EXPORT const unsigned char NetWorkServcieRequestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetWorkServcieRequest/PublicHeader.h>
+
+
